b34353a5f6c29ac011fa54451ead69adbe8d27f4
Ting-Rui, Wang <qwe50120@gmail.com>
2019-04-08 17:50:30 +0000

    Write code in cleaner way.

---
+++ src/parser/generator.h
@@ -0,0 +1,52 @@
+#include "clang/AST/AST.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/CompilationDatabase.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/AST/RecordLayout.h"
+
+#include "config.h"
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace std;
+using namespace llvm;
+
+class ParserHandler : public MatchFinder::MatchCallback {
+private:
+    CompilerInstance &Instance;
+    ASTContext *context;
+
+public:
+    ParserHandler(CompilerInstance &Instance) : Instance(Instance) {}
+    void setContext(ASTContext &context);
+    // implement node handler here
+    virtual void run(const MatchFinder::MatchResult &result);
+};
+
+class ParserASTConsumer : public clang::ASTConsumer {
+public:
+    // define match node type in this function
+    // use matcher.addMatcher()
+    ParserASTConsumer(CompilerInstance &Instance);
+
+private:
+    MatchFinder matcher;
+    ParserHandler handlerForMatchResult;
+
+    void HandleTransitionUnit(ASTContext &context);
+};
+
+class ParserAction : public clang::ASTFrontendAction {
+public:
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+            CompilerInstance &Instance, StringRef InFile) {
+        return std::unique_ptr<ParserASTConsumer>(
+            new ParserASTConsumer(Instance)
+        );
+    }
+};
+
+
+void genParseResult(int argc, const char** argv, Config &config);
---
+++ src/parser/test.cpp
@@ -0,0 +1,16 @@
+struct st{
+    char a;
+    virtual int v_a() {return 1;}
+    int i_a;
+    float z() {return 0;}
+    int zz() {return 1;}
+};
+
+int a() {
+    return 5;
+}
+
+int main() {
+    
+    return 0;
+}
